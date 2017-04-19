19c5d0b87042a0433c6227a31080ddc05ee3fc84
Ubuntu <ubuntu@ip-172-31-3-44.us-west-2.compute.internal>
2017-04-19 20:05:03 +0000

    adding required time parsing script

---
+++ get_time.sh
@@ -0,0 +1,8 @@
+#!bin/bash
+
+multichain-cli chain1 liststreamkeyitems localstream $1 | grep 'key\|data\|blocktime' >time.txt && sed -i 's/ //g' time.txt
+
+tac time.txt | grep '00000000' -m 1 -B 1 | tac | grep blocktime>time1.txt
+
+echo "executed shell script"
+
